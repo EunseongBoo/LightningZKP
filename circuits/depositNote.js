9256c233533b4f5f3b3ca2979a12567a23705475
EunSeong Boo <bes2583@ajou.ac.kr>
2019-09-17 01:38:43 -0700

    modify a depositNote.js file

--- circuits/depositNote.js
+++ circuits/depositNote.js
@@ -8,14 +8,14 @@ const SCALING_FACTOR = new BN('1000000000000000000'); //10**18;
 function getPublicKeyHash(pk) {
   const buf = Buffer.from(pk, 'hex');
   const digest = crypto.createHash('sha256').update(buf).digest('hex');
-  console.log('pk digest', digest);
+  console.log('PK Hash('+pk+') :', digest);
   return digest
 }
 function getNoteHash(encodedNote) {
   const buf = Buffer.from(encodedNote, 'hex');
   const digest = crypto.createHash('sha256').update(buf).digest('hex');
-  console.log('digest', digest);
-  console.log('digest.length', digest.length); //64 hex, 32 bytes
+  console.log('Note Hash:', digest);
+  //console.log('digest.length', digest.length); //64 hex, 32 bytes
   // split into 128 bits each
   return [digest.slice(0, 32), digest.slice(32)] //16 bytes, 16 bytes
 }
@@ -29,24 +29,61 @@ function printZokratesCommand(params) {
   console.log(cmd);
 }
 
-function getCreateNoteParams(_pubKey, _secKey,_value, _nonce) {
+function getCreateNoteParams(_spk, _ssk, _mpk, _rpk, _ovalue, _nvalue, _notenum, _ononce, _snonce0, _snonce1, _rnonce0, _rnonce1, _cnonce) {
   console.log(arguments)
-  let pubKey = new BN(_pubKey, 16).toString(16); // 64 bytes = 512 bits
-  let secKey = new BN(_secKey, 16).toString(16);
-  //console.log('pubKey: ', pubKey.length);
-  console.log()
-  let value = new BN(_value, 16).mul(SCALING_FACTOR).toString(16, 32); // 16 bytes = 128 bits
-  //console.log('value: ', value);
-  //console.log('value.length: ', value.length);
-  let nonce = new BN(_nonce, 16).toString(16, 32); // 16 bytes = 128 bits
-  //console.log('nonce: ', nonce);
-  //let privateParams = [pubKey.slice(0, 32), pubKey.slice(32), nonce];
-  let privateParams = [pubKey.slice(0, 32), pubKey.slice(32, 64), pubKey.slice(64,96), pubKey.slice(96), nonce, secKey];
+  let spk = new BN(_spk, 16).toString(16); // 64 bytes = 512 bits, sender's public key = spk
+  let ssk = new BN(_ssk, 16).toString(16); //sender's secret key = ssk
+  let mpk = new BN(_mpk, 16).toString(16);
+  let rpk = new BN(_rpk, 16).toString(16);
+  console.log('spk: ', spk);
+  console.log('mpk: ', mpk);
+  console.log('rpk: ', rpk);
+
+  //original note's value, new note's value, notenum
+  let ovalue = new BN(_ovalue, 16).mul(SCALING_FACTOR).toString(16, 32); // 16 bytes = 128 bits
+  let nvalue = new BN(_nvalue, 16).mul(SCALING_FACTOR).toString(16, 32); // new deposit note's value
+  let notenum = new BN(_notenum, 16).mul(SCALING_FACTOR).toString(16, 32);
+
+  //nonce
+  let ononce = new BN(_ononce, 16).toString(16, 32); // 16 bytes = 128 bits, original note's nonce
+  let snonce1 = new BN(_snonce0, 16).toString(16, 32); // sender note's nonce
+  let snonce2 = new BN(_snonce1, 16).toString(16, 32);
+  let rnonce1 = new BN(_rnonce0, 16).toString(16, 32); // receiver note's nonce
+  let rnonce2 = new BN(_rnonce1, 16).toString(16, 32);
+  let cnonce = new BN(_cnonce, 16).toString(16, 32); //change note's nonce
+
+  // calcuale public key hash
+  let spkHash = getPublicKeyHash(spk.slice(0, 32) + spk.slice(32, 64) + spk.slice(64,96) + spk.slice(96));
+  let mpkHash = getPublicKeyHash(mpk.slice(0, 32) + mpk.slice(32, 64) + mpk.slice(64,96) + mpk.slice(96));
+  let rpkHash = getPublicKeyHash(rpk.slice(0, 32) + rpk.slice(32, 64) + rpk.slice(64,96) + rpk.slice(96));
+
+  //calculate change
+  let cvalue = ovalue - nvalue*notenum;
+
+  // create note
+  let onote = spkHash + ovalue + ononce;
+  let snote1 = mpkHash + nvalue + snonce1;
+  let snote2 = mpkHash + nvalue + snonce2;
+  let rnote1 = rpkHash + nvalue + rnonce1;
+  let rnote2 = rpkHash + nvalue + rnonce2;
+  let cnote = spkHash + cvalue +cnonce;
+
+  //calculate notes hash
+  let oh = getNoteHash(onote);
+  let sh1 = getNoteHash(snote1);
+  let sh2 = getNoteHash(snote2);
+  let rh1 = getNoteHash(rnote1);
+  let rh2 = getNoteHash(rnote2);
+  let ch = getNoteHash(cnote);
+
+  let publicParams = [oh, sh1, sh2, rh1, rh2, ch, mpk.slice(0,32), mpk.slice(32, 64), mpk.slice(64, 96), mpk.slice(96), notenum, nvalue];
+  let privateParams = [ovalue, ononce, snonce1, snonce2, rnonce1, rnonce2, cnonce, spk.slice(0,32), spk.slice(32,64), spk.slice(64,96), spk.slice(96), ssk, rpk.slice(0,32), rpk.slice(32,64), rpk.slice(64,96), rpk.slice(96)]
+  //let privateParams = [pubKey.slice(0, 32), pubKey.slice(32, 64), pubKey.slice(64,96), pubKey.slice(96), ononce, secKey];
   //let privateParams = [pubKey.slice(0, 64), pubKey.slice(64), nonce];
-  let pubKeyHash = getPublicKeyHash(pubKey.slice(0, 32) + pubKey.slice(32, 64) + pubKey.slice(64,96) + pubKey.slice(96));
-  let note = pubKeyHash + value + nonce;
-  console.log('note', note);
-  let publicParams = getNoteHash(note).concat(value);
+  //let pubKeyHash = getPublicKeyHash(pubKey.slice(0, 32) + pubKey.slice(32, 64) + pubKey.slice(64,96) + pubKey.slice(96));
+  //let onote = pubKeyHash + ovalue + ononce;
+  //console.log('original note', onote);
+  //let publicParams = getNoteHash(onote).concat(ovalue);
   printZokratesCommand(publicParams.concat(privateParams));
 
   /*
