6156b47579a437548613b929034bee6f1ae56d92
Eunseong Boo <bes2583@ajou.ac.kr>
2019-12-17 19:35:54 +0900

    pi testing script and result

---
+++ circuits/pi/pi_test_all.sh
@@ -0,0 +1,13 @@
+./pi_test_createNote_witness.sh
+./pi_test_createNote_proof.sh
+./pi_test_spendNote_witness.sh
+./pi_test_spendNote_proof.sh
+./pi_test_depositNote_witness.sh
+./pi_test_depositNote_proof.sh
+
+cat pi_create_proof.txt | cut -d"m" -f2 | cut -d"s" -f1 | tee -a pi_create_proof_parsing.txt
+cat pi_create_witness.txt | cut -d"m" -f2 | cut -d"s" -f1 | tee -a pi_create_witness_parsing.txt
+cat pi_spend_proof.txt | cut -d"m" -f2 | cut -d"s" -f1 | tee -a pi_spend_proof_parsing.txt
+cat pi_spend_witness.txt | cut -d"m" -f2 | cut -d"s" -f1 | tee -a pi_spend_witness_parsing.txt
+cat pi_deposit_proof.txt | cut -d"m" -f2 | cut -d"s" -f1 | tee -a pi_deposit_proof_parsing.txt
+cat pi_deposit_witness.txt | cut -d"m" -f2 | cut -d"s" -f1 | tee -a pi_deposit_witness_parsing.txt
