be1d6da4adcf86d6d328bdcc93309b90c8023bc7
Eunseong Boo <bes2583@ajou.ac.kr>
2019-12-18 13:21:43 +0900

    pi liquidate testing result update

--- circuits/pi/pi_test_liq.sh
+++ circuits/pi/pi_test_liq.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-./test_liquidateNote_witness.sh
-./test_liquidateNote_proof.sh
+./pi_test_liquidateNote_witness.sh
+./pi_test_liquidateNote_proof.sh
 
 
 cat pi_liquidate_proof.txt | cut -d"m" -f2 | cut -d"s" -f1 | tee -a pi_liquidate_proof_parsing.txt
