331a23359183d21e24994c02780da40825d56f2f
EunSeong Boo <bes2583@ajou.ac.kr>
2020-03-23 01:16:35 -0700

    time result files are updated to time_result folder

--- circuits/test_all.sh
+++ circuits/test_all.sh
@@ -1,16 +1,3 @@
-./mint/test_mint_witness.sh
-./mint/test_mint_proof.sh
-./pour/test_pour_witness.sh
-./pour/test_pour_proof.sh
-./pour4/test_pour4_witness.sh
-./pour4/test_pour4_proof.sh
-./lzkp/test_lzkp_witness.sh
-./lzkp/test_lzkp_proof.sh
-./burn/test_burn_proof.sh
-./burn/test_burn_witness.sh
-./burn4/test_burn4_proof.sh
-./burn4/test_burn4_witness.sh
-
 cat ./mint/time_mint_proof.txt | cut -d" " -f2 | cut -d"s" -f1 | tee -a mint_proof_parsing.txt
 cat ./mint/time_mint_witness.txt | cut -d" " -f2 | cut -d"s" -f1 | tee -a mint_witness_parsing.txt
 cat ./pour/time_pour_proof.txt | cut -d" " -f2 | cut -d"s" -f1 | tee -a pour_proof_parsing.txt
