9bb9f49b8bda70caaf02498fa802c51723131c51
Eunseong Boo <bes2583@ajou.ac.kr>
2020-03-25 01:46:00 +0900

    time evaluations for pi are updated

--- circuits/pi/pi_test_liq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-./pi_test_liquidateNote_witness.sh
-./pi_test_liquidateNote_proof.sh
-
-
-cat pi_liquidate_proof.txt | cut -d"m" -f2 | cut -d"s" -f1 | tee -a pi_liquidate_proof_parsing.txt
-cat pi_liquidate_witness.txt | cut -d"m" -f2 | cut -d"s" -f1 | tee -a pi_liquidate_witness_parsing.txt
--- circuits/pi/test.sh
+++
@@ -1,1 +0,0 @@
-cat pi_create_proof.txt | cut -d"	" -f2 | cut -d"s" -f1 | tee -a pi_parsing_test.txt
